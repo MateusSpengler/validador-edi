88e2d53cbd4ea98f4130fa2b0072f4e0acdb7c38
Mateus <spenglerback86@gmail.com>
2022-12-25 21:38:11 -0300

    teste4

--- src/home/pages/Home.js
+++ src/home/pages/Home.js
@@ -1,131 +1,164 @@
 import * as React from "react";
-import './Home.css';
-import '../javascript/HomeValidador';
+import "./Home.css";
+import "../javascript/HomeValidador";
 import { useForm } from "react-hook-form";
-import Lince from '../images/lince.png';
+import Lince from "../images/lince.png";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import FileDownloadRoundedIcon from '@mui/icons-material/FileDownloadRounded';
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import FileDownloadRoundedIcon from "@mui/icons-material/FileDownloadRounded";
 import { Button } from "@mui/material";
 
 export default function Home() {
+  const { register, handleSubmit } = useForm();
 
-    const { register, handleSubmit } = useForm();
+  const [fatura, setFatura] = React.useState(6);
+  const [texto, setTexto] = React.useState(false);
+  const [conhecimento, setConhecimento] = React.useState(6);
+  const [documento, setDocumento] = React.useState();
 
-    const [fatura, setFatura] = React.useState(6);
-    const [conhecimento, setConhecimento] = React.useState(6);
-    const [documento, setDocumento] = React.useState();
+  const onSubmit = async (data) => {
+    console.log(data.documento.split("\n")[1]);
+  };
 
-    const onSubmit = async data => {
-        console.log(data.documento.split('\n')[1]);
-    }
+  const handleChangeFatura = (event) => {
+    setFatura(event.target.value);
+  };
 
-    const handleChangeFatura = (event) => {
-        setFatura(event.target.value);
+  const handleChangeConhecimento = (event) => {
+    setConhecimento(event.target.value);
+  };
+
+  const handleChangeDocumento = (event) => {
+    setDocumento(event.target.value);
+  };
+
+  const handleChangeArquivo = () => {
+    var preview = document.getElementById("edi-text");
+    var file = document.querySelector("input[type=file]").files[0];
+    console.log(file);
+    var reader = new FileReader();
+
+    var textFile = /text.*/;
+
+    reader.onload = function (event) {
+      preview.innerHTML = event.target.result;
+      setTexto(true);
     };
+    reader.readAsText(file);
+  };
 
-    const handleChangeConhecimento = (event) => {
-        setConhecimento(event.target.value);
-    };
+  return (
+    <div>
+      <Box className="header">
+        <Box className="nav_bar">
+          <div className="title_nav_bar">
+            <div className="logo_div_nav_bar">
+              <img src={Lince} className="logo_nav_bar" alt="logo_lince"></img>
+            </div>
+            <Typography
+              className="teste"
+              sx={{ fontSize: "16pt", fontWeight: "700" }}
+            >
+              Validar documetos de EDI
+            </Typography>
+          </div>
+          <Button sx={{ borderRadius: "8px" }}>
+            <DarkModeOutlinedIcon />
+          </Button>
+        </Box>
+      </Box>
 
-    const handleChangeDocumento = (event) => {
-        setDocumento(event.target.value);
-    }
-
-    const handleChangeArquivo = (event) => {
-        var reader = new FileReader();
-        reader.readAsText(event.target.value, "UTF-8");
-        reader.onload = function (evt) {
-            console.log(evt.target.result);
-            setDocumento(evt.target.result);
-        }
-
-
-    }
-
-    return (
-        <div>
-            <Box className="header">
-                <Box className="nav_bar">
-                    <div className="title_nav_bar">
-                        <div className="logo_div_nav_bar">
-                            <img src={Lince} className="logo_nav_bar" alt="logo_lince"></img>
-                        </div>
-                        <Typography className="teste" sx={{ fontSize: '16pt', fontWeight: '700' }}>Validar documetos de EDI</Typography>
-                    </div>
-                    <Button sx={{ borderRadius: '8px' }}>
-                        <DarkModeOutlinedIcon />
-                    </Button>
-                </Box>
-            </Box>
-
-            <Box className="main">
-                <Box component="form" autoComplete="off" className="content" display={'grid'} onSubmit={handleSubmit(onSubmit)}>
-                    <div className="content_top">
-                        <div className="title_content_top">
-                            <Typography sx={{ fontSize: '20pt', fontWeight: '550' }}>Consultar documentos</Typography>
-                        </div>
-                        <div className="form_content_top">
-                            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                                <InputLabel id="select-fatura">Fatura</InputLabel>
-                                <Select
-                                    {...register('fatura')}
-                                    value={fatura}
-                                    label="Fatura"
-                                    sx={{ backgroundColor: 'white' }}
-                                    onChange={handleChangeFatura}
-
-                                >
-                                    <MenuItem value={6}>Tamanho 6</MenuItem>
-                                    <MenuItem value={7}>Tamanho 7</MenuItem>
-                                    <MenuItem value={8}>Tamanho 8</MenuItem>
-                                </Select>
-                            </FormControl>
-                            <FormControl sx={{ m: 1, minWidth: 150 }} size="small">
-                                <InputLabel id="select-conhecimento">Conhecimento</InputLabel>
-                                <Select
-                                    {...register('conhecimento')}
-                                    value={conhecimento}
-                                    label="Conhecimento"
-                                    sx={{ backgroundColor: 'white' }}
-                                    onChange={handleChangeConhecimento}
-                                >
-                                    <MenuItem value={6}>Tamanho 6</MenuItem>
-                                    <MenuItem value={7}>Tamanho 7</MenuItem>
-                                    <MenuItem value={8}>Tamanho 8</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </div>
-                    </div>
-                    <div className="textField_content_center">
-                        <TextField
-                            {...register('documento')}
-                            value={documento}
-                            id="edi-text"
-                            label="Electronic Data Interchange (EDI)"
-                            multiline
-                            rows={20}
-                            placeholder="Digite o documento..."
-                            sx={{ width: '100%', backgroundColor: 'white' }}
-                            onChange={handleChangeDocumento}
-                        />
-                    </div>
-                    <div className="textField_content_bottom">
-                        <Button variant="outlined" component="label" sx={{ backgroundColor: 'white' }}>
-                            <FileDownloadRoundedIcon />
-                            <Typography>Importar</Typography>
-                            <input hidden accept=".txt" multiple type="file" onChange={handleChangeArquivo} />
-                        </Button>
-                        <Button variant="contained" sx={{ fontWeight: '700' }} type='submit'>Validar</Button>
-                    </div>
-                </Box>
-            </Box>
-        </div>
-    );
+      <Box className="main">
+        <Box
+          component="form"
+          autoComplete="off"
+          className="content"
+          display={"grid"}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="content_top">
+            <div className="title_content_top">
+              <Typography sx={{ fontSize: "20pt", fontWeight: "550" }}>
+                Consultar documentos
+              </Typography>
+            </div>
+            <div className="form_content_top">
+              <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                <InputLabel id="select-fatura">Fatura</InputLabel>
+                <Select
+                  {...register("fatura")}
+                  value={fatura}
+                  label="Fatura"
+                  sx={{ backgroundColor: "white" }}
+                  onChange={handleChangeFatura}
+                >
+                  <MenuItem value={6}>Tamanho 6</MenuItem>
+                  <MenuItem value={7}>Tamanho 7</MenuItem>
+                  <MenuItem value={8}>Tamanho 8</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl sx={{ m: 1, minWidth: 150 }} size="small">
+                <InputLabel id="select-conhecimento">Conhecimento</InputLabel>
+                <Select
+                  {...register("conhecimento")}
+                  value={conhecimento}
+                  label="Conhecimento"
+                  sx={{ backgroundColor: "white" }}
+                  onChange={handleChangeConhecimento}
+                >
+                  <MenuItem value={6}>Tamanho 6</MenuItem>
+                  <MenuItem value={7}>Tamanho 7</MenuItem>
+                  <MenuItem value={8}>Tamanho 8</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+          </div>
+          <div className="textField_content_center">
+            <TextField
+              {...register("documento")}
+              value={documento}
+              id="edi-text"
+              label="Electronic Data Interchange (EDI)"
+              multiline
+              focused={texto}
+              rows={20}
+              placeholder="Digite o documento..."
+              sx={{ width: "100%", backgroundColor: "white" }}
+              onChange={handleChangeDocumento}
+            />
+          </div>
+          <div className="textField_content_bottom">
+            <Button
+              variant="outlined"
+              component="label"
+              sx={{ backgroundColor: "white" }}
+            >
+              <FileDownloadRoundedIcon />
+              <Typography>Importar</Typography>
+              <input
+                hidden
+                accept=".txt"
+                multiple
+                type="file"
+                onChange={handleChangeArquivo}
+              />
+            </Button>
+            <Button
+              variant="contained"
+              sx={{ fontWeight: "700" }}
+              type="submit"
+            >
+              Validar
+            </Button>
+          </div>
+        </Box>
+      </Box>
+    </div>
+  );
 }
