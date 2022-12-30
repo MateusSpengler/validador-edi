8f9aac0a6860e3c6e6941a2017bf2f2ccbd54d6a
LogExp <logexplince@gmail.com>
2022-12-30 09:49:56 -0300

    validações

--- src/home/pages/Home.js
+++ src/home/pages/Home.js
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import NavigationIcon from '@mui/icons-material/Navigation';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeRounded';
-import PriorityHighRoundedIcon from '@mui/icons-material/PriorityHighRounded';
 import { Button, Card, Divider, Select, FormControl, MenuItem, InputLabel, TextField, Typography, Box, Fab } from "@mui/material";
 import Lince from "../images/lince.png";
 import Calendar from "../images/calendar.jpg"
@@ -22,17 +21,80 @@ export default function Home() {
     const [documento, setDocumento] = React.useState();
     const [auxiliar, setAuxiliar] = React.useState([]);
     const [edi, setEdi] = useState([]);
-    const [pageYPosition, setPageYPosition] = useState(0);
 
-    function getPageYAfterScroll() {
-        // setPageYPosition(window.scrollY);
+    function validarCNPJ(cnpj) {
+
+        cnpj = cnpj.replace(/[^\d]+/g, '');
+
+
+
+        if (cnpj === '') return false;
+
+        if (cnpj.length !== 14) {
+            console.log('1');
+            return false;
+        }
+
+
+        // Elimina CNPJs invalidos conhecidos
+        if (cnpj === "00000000000000" ||
+            cnpj === "11111111111111" ||
+            cnpj === "22222222222222" ||
+            cnpj === "33333333333333" ||
+            cnpj === "44444444444444" ||
+            cnpj === "55555555555555" ||
+            cnpj === "66666666666666" ||
+            cnpj === "77777777777777" ||
+            cnpj === "88888888888888" ||
+            cnpj === "99999999999999") {
+            console.log('2');
+            return false;
+        }
+
+
+        // Valida DVs
+        var tamanho = cnpj.length - 2
+        var numeros = cnpj.substring(0, tamanho);
+        var digitos = cnpj.substring(tamanho);
+        var soma = 0;
+        var pos = tamanho - 7;
+        for (var i = tamanho; i >= 1; i--) {
+            soma += numeros.charAt(tamanho - i) * pos--;
+            if (pos < 2)
+                pos = 9;
+        }
+        var resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        console.log('resultado: ' + resultado)
+        console.log('chart: ' + digitos.charAt(0))
+        if (parseInt(resultado) !== parseInt(digitos.charAt(0))) {
+            console.log('3');
+            return false;
+        }
+
+
+        tamanho = tamanho + 1;
+        numeros = cnpj.substring(0, tamanho);
+        soma = 0;
+        pos = tamanho - 7;
+        for (i = tamanho; i >= 1; i--) {
+            soma += numeros.charAt(tamanho - i) * pos--;
+            if (pos < 2)
+                pos = 9;
+        }
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (parseInt(resultado) !== parseInt(digitos.charAt(1))) {
+            console.log('4');
+            return false;
+        }
+
+        return true;
     }
 
-    window.addEventListener('scroll', getPageYAfterScroll);
+    function checarData(data) {
+        return data instanceof Date && !isNaN(data);
+    }
 
     const onSubmit = (data) => {
-
-        setPageYPosition(window.scrollY);
 
         const textoGeral = data.documento.trim().split("\n");
         const tamanhoGeral = data.documento.trim().split("\n").length;
@@ -40,15 +102,6 @@ export default function Home() {
         const tamanhoConhecimento = data.conhecimento;
 
         setAuxiliar([])
-
-        function checarData(data) {
-            return data instanceof Date && !isNaN(data);
-        }
-
-        var x = new Date("01/13/2022");
-        var y = new Date();
-        console.log(checarData(x)); // false
-        console.log(checarData(y)); // true
 
         if (textoGeral[tamanhoGeral - 1].length >= 43 && textoGeral[tamanhoGeral - 1].length <= 45) {
 
@@ -73,6 +126,9 @@ export default function Home() {
             var notasResumo = parseInt(tamanhoGeral) - 1;
             var cnpjAuxiliar = textoGeral[tamanhoGeral - 1].substring(tamanhoAuxiliar1, tamanhoAuxiliar2);
             var cnpjResumo = cnpjAuxiliar.substring(0, 2) + "." + cnpjAuxiliar.substring(2, 5) + "." + cnpjAuxiliar.substring(5, 8) + "/" + cnpjAuxiliar.substring(8, 12) + "-" + cnpjAuxiliar.substring(12, 14);
+
+            var cnpjCheckIndicadorResumo = validarCNPJ(cnpjAuxiliar);
+
             tamanhoAuxiliar1 = tamanhoAuxiliar2;
             tamanhoAuxiliar2 = parseInt(tamanhoAuxiliar2) + parseInt(tamanhoFatura);
             var boletoResumo = textoGeral[tamanhoGeral - 1].substring(tamanhoAuxiliar1, tamanhoAuxiliar2);
@@ -93,7 +149,7 @@ export default function Home() {
             tamanhoAuxiliar2 = textoGeral[tamanhoGeral - 1].length;
             var valorTotalFaturaResumo = textoGeral[tamanhoGeral - 1].substring(tamanhoAuxiliar1, tamanhoAuxiliar2);
 
-            auxiliar.push({ id: 0, quantidadeNotas: notasResumo, edi: textoGeral[tamanhoGeral - 1], indicador: indicadorResumo, descricaoIndicador: descricaoIndicadorResumo, cnpj: cnpjResumo, boleto: boletoResumo, data: dataVencimentoResumo, dataCheckIndicador: dataCheckIndicadorResumo, valorTotal: valorTotalFaturaResumo });
+            auxiliar.push({ id: 0, quantidadeNotas: notasResumo, edi: textoGeral[tamanhoGeral - 1], indicador: indicadorResumo, descricaoIndicador: descricaoIndicadorResumo, cnpj: cnpjResumo, cnpjCheckIndicador: cnpjCheckIndicadorResumo, boleto: boletoResumo, data: dataVencimentoResumo, dataCheckIndicador: dataCheckIndicadorResumo, valorTotal: valorTotalFaturaResumo });
 
             // CARREGANDO DADOS DAS NOTAS
 
@@ -124,6 +180,8 @@ export default function Home() {
                     var indicadorNota = textoGeral[i].substring(0, 1);
                     var cnpjAuxiliarNota = textoGeral[i].substring(1, 15);
                     var cnpjNota = cnpjAuxiliarNota.substring(0, 2) + "." + cnpjAuxiliarNota.substring(2, 5) + "." + cnpjAuxiliarNota.substring(5, 8) + "/" + cnpjAuxiliarNota.substring(8, 12) + "-" + cnpjAuxiliarNota.substring(12, 14);
+                    var cnpjCheckIndicadorNota = validarCNPJ(cnpjAuxiliarNota);
+                    console.log('OPA: ' + cnpjCheckIndicadorNota)
                     tamanhoAuxiliar2 = parseInt(tamanhoFatura) + 15;
                     var boletoNota = textoGeral[i].substring(15, tamanhoAuxiliar2);
                     tamanhoAuxiliar1 = 15;
@@ -178,7 +236,7 @@ export default function Home() {
                     tamanhoAuxiliar2 = parseInt(tamanhoAuxiliar2) + parseInt(6);
                     var notaFiscalNota = textoGeral[i].substring(tamanhoAuxiliar1, tamanhoAuxiliar2);
 
-                    auxiliar.push({ id: y, edi: textoGeral[i], indicador: indicadorNota, descricaoIndicador: descricaoIndicadorNota, cnpj: cnpjNota, boleto: boletoNota, indicadorDevolucao: indicadorDevolucaoNota, descricaoIndicadorDevolucao: descricaoIndicadorDevolucaoNota, serieFiscais: serieFiscaisNota, cfop: cfopNota, ocorrenciaCfop: ocorrenciaCfopNota, conhecimento: conhecimentoNota, dataEmissao: dataEmissaoNota, dataCheckIndicador: dataCheckIndicadorNota, valorFrete: valorFreteNota, valorDesconto: valorDescontoNota, valorAbatimento: valorAbatimentoNota, valorAcrescimo: valorAcrescimoNota, valorBaseIcms: valorBaseIcmsNota, valorIcms: valorIcmsNota, porcentagemAliquota: porcentagemAliquotaNota, notaFiscal: notaFiscalNota });
+                    auxiliar.push({ id: y, edi: textoGeral[i], indicador: indicadorNota, descricaoIndicador: descricaoIndicadorNota, cnpj: cnpjNota, cnpjCheckIndicador: cnpjCheckIndicadorNota, boleto: boletoNota, indicadorDevolucao: indicadorDevolucaoNota, descricaoIndicadorDevolucao: descricaoIndicadorDevolucaoNota, serieFiscais: serieFiscaisNota, cfop: cfopNota, ocorrenciaCfop: ocorrenciaCfopNota, conhecimento: conhecimentoNota, dataEmissao: dataEmissaoNota, dataCheckIndicador: dataCheckIndicadorNota, valorFrete: valorFreteNota, valorDesconto: valorDescontoNota, valorAbatimento: valorAbatimentoNota, valorAcrescimo: valorAcrescimoNota, valorBaseIcms: valorBaseIcmsNota, valorIcms: valorIcmsNota, porcentagemAliquota: porcentagemAliquotaNota, notaFiscal: notaFiscalNota });
 
                     y++;
 
@@ -330,13 +388,13 @@ export default function Home() {
                                 <Box>
                                     <img alt="calendario" className="main_card_top_data_img" src={Calendar} />
                                 </Box>
-                                {edi[0].dataCheckIndicador == true ? (<Typography sx={{fontWeight: 'bold'}}>{edi[0].data}</Typography>) : (<Typography sx={{ color: 'red', fontWeight: 'bold' }}>{edi[0].data}</Typography>)}
+                                {edi[0].dataCheckIndicador === true ? (<Typography sx={{ fontWeight: 'bold' }}>{edi[0].data}</Typography>) : (<Typography sx={{ color: '#C41631', fontWeight: 'bold' }}>{edi[0].data}</Typography>)}
                             </Card>
                             <Card className="main_card_top_infos">
                                 <Box sx={{ display: 'grid', justifyContent: 'center' }}>
                                     <Box sx={{ display: 'flex', textAlign: 'center' }}>
                                         <Typography sx={{ fontSize: '13pt' }}><b>Boleto: {edi[0].boleto}</b></Typography>
-                                        <Typography sx={{ fontSize: '13pt', marginLeft: '15px' }}><b>CNPJ: {edi[0].cnpj}</b></Typography>
+                                        {edi[0].cnpjCheckIndicador === true ? (<Typography sx={{ fontSize: '13pt', marginLeft: '15px' }}><b>CNPJ: {edi[0].cnpj}</b></Typography>) : (<Typography sx={{ color: '#C41631', fontSize: '13pt', marginLeft: '15px' }}><b>CNPJ: {edi[0].cnpj}</b></Typography>)}
                                     </Box>
                                     <Box>
                                         <Typography sx={{ fontSize: '16pt', textAlign: 'center' }}><b>Valor Total: {formatter.format(parseInt(edi[0].valorTotal) / 100)}</b></Typography>
@@ -351,15 +409,19 @@ export default function Home() {
                             {key.id === 0 ? <></> : (
                                 <Card key={key.id} className="main_card_content">
 
-                                    <Box className="card_content_titulo_id">{key.id}</Box>
+                                    {key.cnpjCheckIndicador === true && key.dataCheckIndicador === true && key.descricaoIndicador !== "Inválido" ? (<Box className="card_content_titulo_id">{key.id}</Box>) : (<Box sx={{ backgroundColor: '#C41631' }} className="card_content_titulo_id">{key.id}</Box>)}
 
                                     <Box className="card_content_body">
                                         <Box className="card_content_body_info">
-                                            <Box sx={{ width: '20%' }}><b>{key.descricaoIndicador}</b></Box>
+                                            {/* <Box sx={{ width: '20%' }}><b>{key.descricaoIndicador}</b></Box> */}
+
+                                            {key.descricaoIndicador !== "Inválido" ? (<Box sx={{ width: '20%' }}><b>{key.descricaoIndicador}</b></Box>) : (<Box sx={{ width: '20%', color: '#C41631' }}><b>{key.descricaoIndicador}</b></Box>)}
+
                                             <Box sx={{ width: '20%' }}><b>Boleto:</b> {key.boleto}</Box>
                                             <Box sx={{ width: '20%' }}><b>Conhecimento:</b> {key.conhecimento}</Box>
                                             <Box className="card_content_body_infos"><b>Nota Fiscal:</b> {key.notaFiscal}</Box>
-                                            <Box className="card_content_body_infos"><b>CNPJ:</b> {key.cnpj}</Box>
+
+                                            {key.cnpjCheckIndicador === true ? (<Box className="card_content_body_infos"><b>CNPJ:</b> {key.cnpj}</Box>) : (<Box sx={{ color: '#C41631' }} className="card_content_body_infos"><b>CNPJ: {key.cnpj}</b></Box>)}
                                         </Box>
                                         <Divider />
                                         <Box className="card_content_body_info">
@@ -368,7 +430,7 @@ export default function Home() {
                                             <Box sx={{ width: '20%' }}><b>CFOP:</b> {key.cfop}</Box>
                                             <Box className="card_content_body_infos"><b>Ocorrencia: </b> {key.ocorrenciaCfop}</Box>
 
-                                            {key.dataCheckIndicador == true ? (<Box className="card_content_body_infos"><b>Data: </b> {key.dataEmissao}</Box>) : (<Box sx={{ color: 'red', fontWeight: 700 }} className="card_content_body_infos"><b>Data:</b> {key.dataEmissao}</Box>)}
+                                            {key.dataCheckIndicador === true ? (<Box className="card_content_body_infos"><b>Data: </b> {key.dataEmissao}</Box>) : (<Box sx={{ color: '#C41631', fontWeight: 700 }} className="card_content_body_infos"><b>Data:</b> {key.dataEmissao}</Box>)}
 
                                         </Box>
                                         <Divider />
@@ -385,6 +447,7 @@ export default function Home() {
                                             <Box sx={{ width: '20%' }}><b>ICMS:</b> {formatter.format(key.valorIcms / 100)}</Box>
                                         </Box>
                                     </Box>
+                                    {/* <Box sx={{ display: 'flex', width: '1%', backgroundColor: 'red' }}></Box> */}
                                 </Card>
                             )}
 
